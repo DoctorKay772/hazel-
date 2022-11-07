30479cfc6d8f2140a454b03cf91a6b30c2da8c84
DoctorKay772 <hazelseremani@gmail.com>
2022-11-07 06:46:23 -0800

    today we worked on list

---
+++ list.py
@@ -0,0 +1,44 @@
+# list functions 
+a = [1, 2, 3, True]
+b= [2,4,7,9]
+
+# pop()
+a.pop()
+print(a)
+# extend()
+a.extend([3,4,5])
+print(a)    
+# delete()
+del a [2]
+print(a)     
+# clear()
+
+# append()
+a.append(4)
+print(a)   
+# reverse()
+a.reverse()
+print(a)
+# insert()
+a.insert(2, 100)
+print(a)
+# copy()
+c= a.copy()
+print(c)
+# count()
+print(a.count(3))
+# index()
+print(a.index(2))
+# sort()
+b.sort()
+print(b)
+# remove()
+a.remove(1)
+print(a)
+
+
+# length()
+print(len(a))
+# # sum()
+b= sum(a)
+print(b)
